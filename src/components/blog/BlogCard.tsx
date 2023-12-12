5d2301bdd8e0bf9d1aa9433be00228833bab4c20
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-12-12 01:07:42 -0600

    first commit

---
+++ src/components/blog/BlogCard.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  id: string;
+  title: string;
+  href: string;
+  description: string;
+  imageUrl: string;
+  date: string;
+  datetime: string;
+  author: Author;
+}
+
+interface Author {
+  name: string;
+  imageUrl: string;
+}
+
+export const BlogCard = ({
+  id,
+  title,
+  href,
+  description,
+  imageUrl,
+  date,
+  datetime,
+  author,
+}: Props) => {
+  return (
+    <>
+      <article
+        key={id}
+        className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-white px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+      >
+        <Image
+          src={imageUrl}
+          alt=""
+          className="absolute inset-0 -z-10 h-full w-full object-cover"
+          width={500}
+          height={500}
+        />
+        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+        <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+        <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+          <time dateTime={datetime} className="mr-8">
+            {date}
+          </time>
+          <div className="-ml-4 flex items-center gap-x-4">
+            <svg
+              viewBox="0 0 2 2"
+              className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
+            >
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <div className="flex gap-x-2.5">
+              <Image
+                src={author.imageUrl}
+                alt=""
+                className="h-6 w-6 flex-none rounded-full bg-white/10"
+                width={24}
+                height={24}
+              />
+              {author.name}
+            </div>
+          </div>
+        </div>
+        <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
+          <Link href={href}>
+            <span className="absolute inset-0" />
+            {title}
+          </Link>
+        </h3>
+      </article>
+    </>
+  );
+};
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+// Blog
+export * from './blog/BlogGrid';
+export * from './blog/BlogCard';
+
+// Hero
+export * from './hero/HeroSection';
+
+// Shared
+export * from './shared/Footer';
