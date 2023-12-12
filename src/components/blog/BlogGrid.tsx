d2c004583cd29b1ba2850e69993d769a2530c044
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-12-12 01:09:14 -0600

    Se agrego key al BlogGrid

--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -35,6 +35,7 @@ export const BlogGrid = () => {
           <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <BlogCard
+                key={post.id}
                 id={post.id}
                 title={post.title}
                 href={post.href}
