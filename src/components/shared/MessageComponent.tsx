57309bd2b76531a04624b7f9f6f233d297fea58e
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-12-14 16:33:24 -0600

    Se actualizo el diseño del message component

--- src/components/shared/MessageComponent.tsx
+++ src/components/shared/MessageComponent.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 interface NotificationMessage {
@@ -10,23 +9,14 @@ interface NotificationMessage {
 const MessageComponent = ({ topic, message, comment }: NotificationMessage) => {
   return (
     <>
-      <div className="bg-white">
+      <div className="">
         <main className="mx-auto w-full max-w-7xl px-6 lg:px-8">
-          <div className="flex-shrink-0 pt-16">
-            <Image
-              className="mx-auto h-24 w-auto"
-              src="https://raw.githubusercontent.com/imsoft/BlogPosts/main/images/imsoft/logotipo-imsoft-transparente-azul-rectangular.png"
-              alt="imSoft"
-              height={650}
-              width={650}
-            />
-          </div>
           <div className="mx-auto max-w-xl py-16 sm:pb-24">
             <div className="text-center">
-              <p className="text-base font-semibold text-primary-600">
+              <p className="text-base font-semibold text-gray-400">
                 {topic}
               </p>
-              <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">
                 {message}
               </h1>
               <p className="mt-2 text-lg text-gray-500">{comment}</p>
@@ -35,7 +25,7 @@ const MessageComponent = ({ topic, message, comment }: NotificationMessage) => {
               <div className="mt-8">
                 <Link
                   href="/"
-                  className="text-base font-medium text-primary-600 hover:text-primary-500"
+                  className="text-base font-medium text-primary-600 hover:text-gray-500"
                 >
                   Regresar al inicio
                   <span aria-hidden="true"> &rarr;</span>
