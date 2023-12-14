fdf40adbfe849ebe6139d0232e405a008d2ded80
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-12-14 17:01:36 -0600

    Prueba del BlogGrid

--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -32,7 +32,7 @@ const BlogGrid = async () => {
           <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {blogs.map((blog) => (
               <>
-                <BlogCard key={blog.title} blog={blog} />
+                <BlogCard key={blog._id} blog={blog} />
               </>
             ))}
           </div>
